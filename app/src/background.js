3cdf13bdaacc451e791d0851320bc10342384e58
Itachi-MacOS <warrior.006@inbox.ru>
2022-02-27 00:23:54 +0600

    linked vue3 with chrome plugin

---
+++ app/src/background.js
@@ -0,0 +1,8 @@
+chrome.runtime.onMessage.addListener (message => {
+  console.log(message);
+})
+
+// chrome.action (browserAction && pageAction)
+// chrome.runtime
+// chrome.tabs
+// chrome.scripting
--- app/vue.config.js
+++ app/vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pages: {
+    override: {
+      template: 'public/browser-extension.html',
+      entry: './src/override/main.js',
+      title: 'Override'
+    }
+  },
+
+  pluginOptions: {
+    browserExtension: {
+      componentOptions: {
+        background: {
+          entry: 'src/background.js'
+        }
+      }
+    }
+  }
 })
